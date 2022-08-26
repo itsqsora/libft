a54401151bc6d6c9339efdbe6ebb141413832832
Mac <mac@Mac-2.local>
2022-08-26 13:28:40 +0700

    fixed

--- ft_isdigit.c
+++ ft_isdigit.c
@@ -6,15 +6,15 @@
 /*   By: mac <sorungta@42bangkok.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 13:01:18 by mac               #+#    #+#             */
-/*   Updated: 2022/08/26 13:13:04 by mac              ###   ########.fr       */
+/*   Updated: 2022/08/26 13:28:20 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_isdigit(char c)
+int	ft_isdigit(int c)
 {
-	if ( c >= '0' && c <= '9')
+	if (c >= 0 && c <= 9)
 		return (1);
 	return (0);
 }

e31ea8edac9c7d3a1c262364661885aefdb7c610
Mac <mac@Mac-2.local>
2022-08-26 13:39:37 +0700

    char c >> isalpha

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: mac <sorungta@42bangkok.com>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/08/26 13:21:17 by mac               #+#    #+#             */
-/*   Updated: 2022/08/26 13:27:00 by mac              ###   ########.fr       */
+/*   Updated: 2022/08/26 13:39:03 by mac              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@
 # include <unistd.h>
 
 int		ft_isalnum(int c);
-int		ft_isalpha(int c);
+int		ft_isalpha(char c);
 int		ft_isascii(int c);
 int		ft_isdigit(int c);
 int		ft_isprint(int c);
